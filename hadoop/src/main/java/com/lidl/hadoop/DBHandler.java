4dbe9ab565b8c6897f081261cdb49cec6b8317d8
lidaling <lidaling@foxmail.com>
2014-02-25 10:07:26 +0800

    update

--- hadoop/src/main/java/com/lidl/hadoop/DBHandler.java
+++ hadoop/src/main/java/com/lidl/hadoop/DBHandler.java
@@ -4,7 +4,7 @@ package com.lidl.hadoop;
  * Created by worker on 2/24/14.
  */
 public class DBHandler {
-    private static final String connectionStr = "mongodb://zonlolo:zonlolo_P2ssW0rd@192.168.1.100/zonlolo";
+    private static final String connectionStr = "mongodb://zonlolo:zonlolo@192.168.1.100/zonlolo";
     public static String getCollection(String collectionName) {
         return connectionStr.concat(".").concat(collectionName);
     }
